9093917d1a6d85ba9eef5769518d2f5272c739b4
freejacque <freejacque@gmail.com>
2015-07-08 21:46:03 -0400

    user model created, dev and production environments updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # need to change to the real host before going live
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
