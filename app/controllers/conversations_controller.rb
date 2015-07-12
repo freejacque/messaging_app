b60c2329848399656c78f08161a61505dbde60c2
freejacque <freejacque@gmail.com>
2015-07-11 21:20:36 -0400

    added show and get_conversation to conversations_controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,10 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_mailbox
+  before_action :get_conversation, except: [:index]
+
+  def show
+  end
 
   def index
     @conversations = @mailbox.inbox.paginate(page: params[:page], per_page:)
@@ -10,5 +14,9 @@ class ConversationsController < ApplicationController
 
   def get_mailbox
     @mailbox ||= current_user.mailbox
+  end
+
+  def get_conversation
+    @conversation ||= @mailbox.conversations.find(params[:id])
   end
 end
