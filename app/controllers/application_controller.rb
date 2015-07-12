6a7ea22810ec9f1f39580b6347a242dadf6b790e
freejacque <freejacque@gmail.com>
2015-07-11 21:29:54 -0400

    added record not found rules in app_contrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanatizer.for(:sign_up) << :name
     devise_parameter_sanatizer.for(:account_update) << :name
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:warning] = 'Resource not found.'
+    redirect_back_or root_path
+  end
+
+  def redirect_back_or(path)
+    redirect_to request.referer || path
   end
 end
