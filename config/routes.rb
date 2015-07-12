1cd84410bcfefd8f2dfb1180d5fde19018097d15
freejacque <freejacque@gmail.com>
2015-07-11 20:30:55 -0400

    added resources in routes for coversations

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :conversations, only: [:index, :show, :destroy]
 
   # Example resource route with options:
   #   resources :products do
