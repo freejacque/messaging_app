0647abbea4e5dd3707d23ebc9285b92545466b8d
freejacque <freejacque@gmail.com>
2015-08-01 22:39:08 -0400

    altered devise code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanatizer.for(:sign_up) << :name
-    devise_parameter_sanatizer.for(:account_update) << :name
+    devise_parameter_sanatizer.for(:sign_up) << :username
+    devise_parameter_sanatizer.for(:account_update) << :username
   end
 
   rescue_from ActiveRecord::RecordNotFound do
